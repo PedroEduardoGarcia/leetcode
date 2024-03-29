431b3161648d5396b5aa9703072362015ac4424b
Pedro Eduardo Garcia <pedroeduardogarcia@outlook.com>
2024-03-29 13:21:33 -0300

    Solutions for 709 To Lower Case Added py, c++, js and md files
    Signed-off-by: Pedro Eduardo Garcia <pedroeduardogarcia@outlook.com>

---
+++ src/0709-To-Lower-Case/to_lower_case.js
@@ -0,0 +1,13 @@
+/**
+ * @param {string} s
+ * @return {string}
+ */
+var toLowerCase = function(s) {
+  let ans = s.toLowerCase()
+  
+  return ans
+};
+
+let s = "LOVELY"
+
+console.log(toLowerCase(s))
